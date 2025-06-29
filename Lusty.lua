62122ae49228cb4615a47894eddab94d6ef059da
Lusty joker <toby.hayter10@gmail.com>
2025-06-29 11:26:15 +0100

    Update Lusty.lua

--- Lusty.lua
+++ Lusty.lua
@@ -1,4 +1,6 @@
 ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
+lc_atlas = 'hearts_1',
+    hc_atlas = "hearts_2",
 SMODS.Atlas {
     key = 'hearts_1',
     path = "lusty.png",
