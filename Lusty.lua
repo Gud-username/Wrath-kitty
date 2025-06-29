433493ab125cd5d918334d414d93ab30bae179c5
Lusty joker <toby.hayter10@gmail.com>
2025-06-29 12:48:01 +0100

    Update Lusty.lua

--- Lusty.lua
+++ Lusty.lua
@@ -1,32 +1,47 @@
-ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}
-
-lc_atlas = "hearts_1"
-hc_atlas = "hearts_2"
-
-SMODS.Atlas {
-    key = 'hearts_1',
-    path = "lusty.png",
-    atlas_table = 'ASSET_ATLAS',
-    px = 71,
-    py = 95,
-    disable_mipmap = true,
+local atlas_hc = SMODS.Atlas {
+	key = "skin_hc",
+	path = "lusty_hc.png",
+	px = 71,
+	py = 95,
 }
-SMODS.Atlas {
-    key = 'hearts_2',
-    path = "lusty_hc.png",
-    atlas_table = 'ASSET_ATLAS',
-    px = 71,
-    py = 95,
-    disable_mipmap = true,
-}    
-SMODS.DeckSkin({
-    key = 'lusty_hearts',
-    suit = "Hearts",
-    ranks = ranks,
-    lc_atlas = 'vocalatro_hearts_1',
-    hc_atlas = "vocalatro_hearts_2",
-    posStyle = 'collab',
-    loc_txt = {
-        ["en-us"] = "Smol lusty",
-    }
+
+local atlas_lc = SMODS.Atlas {
+	key = "skin_lc",
+	path = "lusty.png",
+	px = 71,
+	py = 95,
+}
+SMODS.DeckSkin {
+	key = "example",
+	suit = "Hearts",
+	loc_txt = "Example",
+	palettes = {
+		{
+			key = 'lc',
+			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			display_ranks = {"King", "Queen", "Jack"},
+			atlas = atlas_lc.key,
+			pos_style = 'deck',
+			suit_icon = {
+				atlas = icon_lc.key,
+			},
+		},
+		{
+			key = 'hc',
+			ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "King", "Ace",},
+			display_ranks = {"King", "Queen", "Jack"},
+			atlas = atlas_hc.key,
+			pos_style = 'deck',
+			colour = HEX("9734f0"),
+			suit_icon = {
+				atlas = icon_hc.key,
+			},
+		},
+	},
+})
+SMODS.Atlas({
+    key = 'modicon',
+    path = 'Icon/Icon.png',
+    px = 34,
+    py = 34
 })
